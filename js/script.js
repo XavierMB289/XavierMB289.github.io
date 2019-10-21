44b0d1f1f2142e009c12998be9c005a35b80593a
XavierMB289 <silverneon123@gmail.com>
2019-10-21 16:13:31 -0400

    taco

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 $(document).ready(function(){
 	$('.sidenav').sidenav();
-	if(getCookie("username") == ""){
+	if(getCookie("username") != ""){
 		$(".userHide").hide();
 	}
 });
