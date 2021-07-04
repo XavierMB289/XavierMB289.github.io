942915f898ef59eb69c693968a8ef05dad7dd0a7
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2021-07-04 18:23:00 -0400

    Update functions.js

--- logicGateGame/js/functions.js
+++ logicGateGame/js/functions.js
@@ -7,7 +7,7 @@ function getLevel(path, callback){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var result = this.responseText;
+			var result = this.responseText.trim();
 			callback(JSON.parse(result));
 		}
 	};
