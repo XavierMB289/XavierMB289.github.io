f436fd6a91478db56041303cda0da2d9c19e9bb5
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2021-07-04 22:26:45 -0400

    Update game.js

--- logicGateGame/js/game.js
+++ logicGateGame/js/game.js
@@ -1,11 +1,13 @@
 //Game Variables
-var unselected, selected;
+var unselected, selected, gatePipe, pipe;
 var userSelect = 0, oldUS; //The keypress is handled based on what this is...
 var level, currentLevel = 1;
 
 function gameInit(){
 	unselected = getImage("img/unselected_slot.png");
 	selected = getImage("img/selected_slot.png");
+	gatePipe = getImage("img/gate_pipe.png");
+	pipe = getImage("img/pipe.png");
 	
 	getLevel("level/"+currentLevel+".json", loadGameLevel);
 }
@@ -33,14 +35,23 @@ function slowGameUpdate(){
 	}
 	ctx.translate(-(canvasW/2-3*selW-3),-(canvasH-selH-10));
 	
+	ctx.translate(canvasW/2-6*selW-6,10);
+	for(var i = 0; i < level.level.length(); i++){
+		var x = level.level[i][2]-1;
+		var y = level.level[i][3]-1;
+		switch(level.level[i][0]){ //Item name
+			case "button":
+				ctx.drawImage(getImage("img/button_off.png"), x*selW+x, y*selH+y);
+				break;
+		}
+	}
 	if(userSelect >= 6){
-		ctx.translate(canvasW/2-6*selW-6,10);
 		var temp = userSelect - 6;
 		var x = temp % 12;
 		var y = Math.floor(temp/12);
 		ctx.drawImage(selected, x*selW+x, y*selH+y);
-		ctx.translate(-(canvasW/2-6*selW-6),-10);
 	}
+	ctx.translate(-(canvasW/2-6*selW-6),-10);
 	
 }
 document.onkeyup = function(e){
