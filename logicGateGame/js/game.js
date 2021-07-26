644a61f2815fd1b646a597f1367bc7be788bed13
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2021-07-26 17:41:01 -0400

    Because I forgot...
    I dont remember how I coded the inventory to read from the level.inv...
    
    I also fixed lvl 2

--- logicGateGame/js/game.js
+++ logicGateGame/js/game.js
@@ -23,7 +23,7 @@ function gameInit(){
 	
 	currentLevel = getCookie("currentLevel");
 	if(currentLevel == ""){
-		currentLevel = 1;
+		currentLevel = 3;
 	}else{
 		currentLevel = parseInt(currentLevel);
 	}
