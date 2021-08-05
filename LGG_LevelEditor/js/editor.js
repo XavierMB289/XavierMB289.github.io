6706222a49a387959b2b8973a0e781553f06e48a
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2021-08-04 21:13:47 -0400

    Level EDITOR
    YAY! You can now build levels! You cannot play them though...

--- LGG_LevelEditor/js/editor.js
+++ LGG_LevelEditor/js/editor.js
@@ -1,17 +1,17 @@
 //Game Variables
 var selected, items;
 var selW, selH;
-var userSelect = 0, oldUS, userItem = null; //The keypress is handled based on what this is... userItem = [name, direction, oldCoords]
+var userSelect = 0, oldUS = null, userItem = null;
 var level; //The new level...
 
 function gameInit(){
 	selected = getImage("img/selected_slot.png");
 	items = {
-		"names": ["andGate", "battery", "buttonSwitch", "pipe"],
+		"names": ["andGate", "battery", "button", "wire"],
 		"andGate": getImage("img/and_gate.png"),
 		"battery": getImage("img/battery_empty.png"),
-		"buttonSwitch": getImage("img/button_off.png"),
-		"pipe": getImage("img/pipe.png")
+		"button": getImage("img/button_off.png"),
+		"wire": getImage("img/pipe.png")
 	};
 	
 	selected.onload = function(){ //allows for download and play on desktop
@@ -37,39 +37,106 @@ function slowGameUpdate(){
 	ctx.fillStyle = "#FFFFFF";
 	
 	ctx.translate(10, canvasH/2-4*selW-4);
+	for(var i = 0; i < level.level.length; i++){
+		var item = level.level[i];
+		var dirs = item[1];
+		var x = item[2];
+		var y = item[3];
+		drawItem(level, level.level[i][0], x, y, dirs);
+	}
 	if(userSelect < 96){
 		var x = userSelect % 12;
 		var y = Math.floor(userSelect/12);
-		ctx.drawImage(selected, x*selW+x, y*selH+y);
 		if(userItem != null){
-			drawItem(userItem[0], x, y, userItem[1]);
+			ctx.drawImage(userItem[1], x*selW+x, y*selH+y);
 		}
+		ctx.drawImage(selected, x*selW+x, y*selH+y);
 	}
 	ctx.translate(-10, -(canvasH/2-4*selW-4));
 	
 	//Drawing All the different usable items in the inventory grid
 	ctx.translate(canvasW-7*selW-7, canvasH/2-4*selW-4);
-	for(var i = 1; i < items.length; i++){
+	for(var i = 0; i < items.names.length; i++){
 		var x = i % 6;
 		var y = Math.floor(i / 6);
-		ctx.drawImage(items[items["names"][i]], x*selW-x, y*selW-y);
+		ctx.drawImage(items[items["names"][i]], x*selW+x, y*selW+y);
+	}
+	if(userSelect > 95){
+		var x = (userSelect-96) % 6;
+		var y = Math.floor((userSelect-96)/6);
+		ctx.drawImage(selected, x*selW+x, y*selH+y);
 	}
 	ctx.translate(-(canvasW-7*selW-7), -(canvasH/2-4*selW-4));
 }
 document.onkeyup = function(e){
 	var letter = e.key.toLowerCase();
-	switch(letter){ //No matter what...
-		case "w": //up
-			userSelect -= userSelect - 12 > -1 ? 12 : 0;
-			break;
-		case "s":
-			userSelect += userSelect + 12 < 96 ? 12 : 0;
-			break;
-		case "a":
-			userSelect -= userSelect % 12 != 0 ? 1 : 0;
-			break;
-		case "d":
-			userSelect += (userSelect + 1) % 12 != 0 ? 1 : 0; 
-			break;
+	if(userSelect < 96){ //On game board...
+		switch(letter){ //No matter what...
+			case "w": //up
+				userSelect -= userSelect - 12 > -1 ? 12 : 0;
+				break;
+			case "s":
+				userSelect += userSelect + 12 < 96 ? 12 : 0;
+				break;
+			case "a":
+				userSelect -= userSelect % 12 != 0 ? 1 : 0;
+				break;
+			case "d":
+				userSelect += (userSelect + 1) % 12 != 0 ? 1 : 0; 
+				break;
+			case "enter":
+				if(userItem != null){
+					if(getItem(level, x, y) != null){
+						removeItem(level, x, y);
+					}
+					var dirs = prompt("DIRS: (n,s,e,w)\nTHE LAST LETTER IS THE OUTPUT\nExample:w,e");
+					var x = userSelect % 12;
+					var y = Math.floor(userSelect/12);
+					var tempArray = [userItem[0], dirs, x, y, false];
+					if(userItem[0]!="wire"){
+						tempArray.push(userItem[1]);
+					}
+					level.level.push(tempArray);
+				}
+				break;
+			case "backspace":
+				if(oldUS != null){
+					var tempUS = userSelect;
+					userSelect = oldUS;
+					oldUS = tempUS;
+				}else{
+					oldUS = userSelect;
+					userSelect = 96;
+				}
+				break;
+		}
+	}else{ //In "inventory"
+		var tempPos = userSelect - 96;
+		switch(letter){ //No matter what...
+			case "w": //up
+				userSelect -= tempPos - 6 > -1 ? 6 : 0;
+				break;
+			case "s":
+				userSelect += tempPos + 6 < 48 ? 6 : 0;
+				break;
+			case "a":
+				userSelect -= tempPos % 6 != 0 ? 1 : 0;
+				break;
+			case "d":
+				userSelect += (tempPos + 1) % 6 != 0 ? 1 : 0; 
+				break;
+			case "enter":
+				if(tempPos < items.names.length){
+					userItem = [items.names[tempPos], items[items.names[tempPos]]];
+				}
+				//NO BREAK!!
+			case "backspace":
+				if(oldUS != null){
+					var tempUS = userSelect;
+					userSelect = oldUS;
+					oldUS = tempUS;
+				}
+				break;
+		}
 	}
 };
--- LGG_LevelEditor/js/loop.js
+++ LGG_LevelEditor/js/loop.js
@@ -5,8 +5,12 @@ var loopDelaySet = 6;
 var loopDelay = loopDelaySet;
 
 window.onload = function(){
-	canvas.width = document.body.clientWidth; //document.width is obsolete
-	canvas.height = document.body.clientHeight; //document.height is obsolete
+	var win = window,
+		doc = document,
+		docElem = doc.documentElement,
+		body = doc.getElementsByTagName('body')[0];
+    canvas.width = win.innerWidth || docElem.clientWidth || body.clientWidth;
+    canvas.height = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
 	canvasW = canvas.width;
 	canvasH = canvas.height;
 	
