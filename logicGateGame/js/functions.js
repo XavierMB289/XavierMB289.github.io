a7c07b09c2f490fed8b54f5925c115ddb6ca83c7
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2022-05-30 01:06:06 -0400

    Testing Cookie Path
    Testing the cookie path

--- logicGateGame/js/functions.js
+++ logicGateGame/js/functions.js
@@ -21,7 +21,7 @@ function setCookie(cname, cvalue, exdays) {
 	const d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
 	let expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=https://xaviermb289.github.io/";
 }
 function getCookie(cname) {
 	let name = cname + "=";
