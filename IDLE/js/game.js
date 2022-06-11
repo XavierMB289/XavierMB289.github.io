39d521fd6871b874e35422b41b3c0bb16c8f9150
XavierMB289 <48699746+XavierMB289@users.noreply.github.com>
2022-06-11 02:13:06 -0400

    Rename

---
+++ IDLE/js/game.js
@@ -0,0 +1,21 @@
+//Window Variables
+var currentPage, nextPage;
+var pages = {
+	INTRO: "intro",
+	MAINMENU: "mainmenu",
+	GAME: "game",
+	CREDITS: "credits"
+};
+//Intro Variables
+var names = ["SilverNeon123", "XavierMB", "CoffeeDev"];
+var namesNum = 0;
+var stringNum = 0;
+function gameInit(){
+	
+}
+function gameUpdate(){
+	
+}
+function slowGameUpdate(){
+	
+}
